533ec79a4f18d7685daec44c5d1e42ac12c384f2
NoamCurely <noamcurely@gmail.com>
2025-02-16 16:17:21 +0100

    mise a jour du message plus custom

--- discord.js
+++ discord.js
@@ -1,5 +1,6 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { DISCORD_TOKEN, CHANNEL_ID } = require('./config');
+const embed = new EmbedBuilder();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -8,20 +9,28 @@ client.once('ready', () => {
 });
 
 function sendDiscordNotification(event) {
-  const channel = client.channels.cache.get(CHANNEL_ID);
-  if (channel) {
-    const streamTitle = event.title || "Live en cours !";
-    const streamUrl = `https://www.twitch.tv/${event.broadcaster_user_login}`;
-
-    const message = `@everyone ${event.broadcaster_user_name} part en live Minecraft juste ici ${streamUrl} !\n` +
-                    `${streamTitle} @imozne_ !discord !config !setup`;
-
-    channel.send(message);
-  } else {
-    console.error("❌ Impossible de trouver le canal Discord.");
-  }
-}
-
+        const channel = client.channels.cache.get(CHANNEL_ID);
+        if (channel) {
+        console.log('Event reçu:', JSON.stringify(event, null, 2));
+          const streamTitle = event.title;
+          const streamUrl = `https://www.twitch.tv/${event.broadcaster_user_login}`;
+          const gameName = event.game_name || "Un jeu génial !";
+      
+          const embed = new EmbedBuilder()
+            .setColor('#9146FF') // Couleur Twitch
+            .setTitle(`${streamTitle}`)
+            .setURL(streamUrl)
+            .setDescription(`${event.broadcaster_user_name} est en live !`)
+            .addFields({ name: '\u200B', value: `🎮 **${gameName}**`, inline: true })
+            .setImage(`https://static-cdn.jtvnw.net/previews-ttv/live_user_${event.broadcaster_user_login}-640x360.jpg`);
+      
+          // Envoi du message avec l'embed
+          channel.send({ content: '@Notif L1', embeds: [embed] });
+        } else {
+          console.error("❌ Impossible de trouver le canal Discord.");
+        }
+      }
+      
 client.login(DISCORD_TOKEN);
 
 module.exports = {
